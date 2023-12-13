5fcb39ec9cf8432ecabaa0101b70e46b4821253a
Samuel W. Flint <swflint@flintfam.org>
2023-12-13 14:33:54 -0600

    plover flake input

--- flake.nix
+++ flake.nix
@@ -4,13 +4,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     utils.url = "github:numtide/flake-utils";
+    plover-flake.url = "github:dnaq/plover-flake";
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, plover-flake }:
     utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        plover-pkgs = plover-flake.packages.${system};
       in
       rec {
         packages = {
@@ -19,6 +21,11 @@
           usbscale = pkgs.callPackage ./usbscale { };
           sctools = pkgs.callPackage ./sctools { };
           dbus-listen = pkgs.callPackage ./dbus-listen { };
+          my_plover = plover-pkgs.plover.with-plugins (ps: with ps; [
+            plover_auto_reconnect_machine
+            plover_console_ui
+            plover_python_dictionary
+          ]);
         };
         defaultPackage = packages.pango-typefaces;
       }
